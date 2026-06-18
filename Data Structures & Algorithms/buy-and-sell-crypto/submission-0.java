205c40a16b9a8c1072bae4fce1f9a4a69ad9ae48
SHRIGANAPATHI V R <shriganapathi.vr@gmail.com>
2026-06-18 07:16:18 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = prices[0];
+        int maxProfit = 0;
+        for(int price : prices){
+            min = Math.min(price,min);
+            maxProfit = Math.max(price - min,maxProfit);
+        }      
+        return maxProfit;
+    }
+}
